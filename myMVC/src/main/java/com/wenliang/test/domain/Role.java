33edb841828d3c4a8e4c27c1d0c58c45d4661752
wenliang <9549260@qq.com>
2019-12-26 15:48:31 +0800

    2019-12-26

---
+++ myMVC/src/main/java/com/wenliang/test/domain/Role.java
@@ -0,0 +1,27 @@
+package com.wenliang.test.domain;
+
+/**
+ * @author wenliang
+ * @date 2019-12-26
+ * 简介：
+ */
+public class Role {
+    private Integer id;
+    private String rolename;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+}
---
+++ myMVC/src/main/java/com/wenliang/test/domain/UserRole.java
@@ -0,0 +1,29 @@
+package com.wenliang.test.domain;
+
+/**
+ * @author wenliang
+ * @date 2019-12-26
+ * 简介：
+ */
+public class UserRole {
+
+    private Integer userid;
+    private Integer roleid;
+
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getRoleid() {
+        return roleid;
+    }
+
+    public void setRoleid(Integer roleid) {
+        this.roleid = roleid;
+    }
+}
